35bf3ddd7f5ad986380b5a9736a59dec0b055769
Adam Konber <konber3@gmail.com>
2024-07-27 09:17:40 -0600

    Created function to fetch brewery data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 const App = () => {
+  const getBeer = () => {
+    fetch('https://api.openbrewerydb.org/v1/breweries')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+
+  }
+
+  useEffect(() => {
+    getBeer()
+  }, [])
   
   return (
     <div className='App'>App</div>
