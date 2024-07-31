15811a99da974bc1ac51b043d7d650d7229290bc
Adam Konber <konber3@gmail.com>
2024-07-31 15:37:03 -0600

    Created BrewContainer component to map through breweries prop and drill values to child brewCard component. plus minor styling

--- src/Components/BrewContainer/BrewContainer.jsx
+++ src/Components/BrewContainer/BrewContainer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../BrewContainer/BrewContainer.css'
+import BrewCard from '../BrewCard/BrewCard'
+import FilterBar from '../FilterBar/FilterBar'
+import { Link } from 'react-router-dom'
 
-const BrewContainer = () => {
+const BrewContainer = ({ breweries }) => {
+    const BreweryCards = breweries.map(brewery => {
+        return (
+            <div key={brewery.id}>
+                <Link to={`/detail/${brewery.id}`}>
+                    <BrewCard 
+                    name={brewery.name}
+                    type={brewery.brewery_type}
+                    city={brewery.city}
+                    /> 
+                </Link>
+            </div> 
+        )
+    })
+
   return (
-    <div>BrewContainer</div>
+    <div>
+    
+    {BreweryCards}
+    </div>
   )
 }
 
