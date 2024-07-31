8c31ce577da83cc74bcbf89de7980b5262ae74b4
Adam Konber <konber3@gmail.com>
2024-07-31 15:44:04 -0600

    Create Hero section w/ basic styling

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../HeroSection/HeroSection.css'
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section'>HeroSection</div>
+  )
+}
+
+export default HeroSection
