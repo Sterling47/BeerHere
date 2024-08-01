875cde150fb0d396b721267408ccc03887893c18
Adam Konber <konber3@gmail.com>
2024-08-01 13:47:48 -0600

    Added functionality to filter through all breweries

--- src/Components/AllBreweryPage/AllBrew.jsx
+++ src/Components/AllBreweryPage/AllBrew.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import BrewCard from '../BrewCard/BrewCard';
+import FilterBar from '../FilterBar/FilterBar';
+import '../AllBreweryPage/AllBrew.css';
 
-const AllBrew = ({ breweries }) => {
-  const groupedBreweries = groupBreweriesByFirstChar(breweries);
+const AllBrew = ({ breweries, onFilter }) => {
+  const [filteredBreweries, setFilteredBreweries] = useState(breweries);
+
+  useEffect(() => {
+    setFilteredBreweries(breweries);
+  }, [breweries]);
+
+  const handleFilter = (filtered) => {
+    setFilteredBreweries(filtered);
+    onFilter(filtered); // Update the filtered breweries in App component as well
+  };
+
+  const groupedBreweries = groupBreweriesByFirstChar(filteredBreweries);
 
   return (
-    <div>
-      {Object.keys(groupedBreweries).sort().map(char => (
-        <div key={char}>
-          <h2>{char.match(/[A-Z]/) ? char : '0-9'}</h2>
-          {groupedBreweries[char].map(brewery => (
-            <BrewCard
-              key={brewery.id}
-              name={brewery.name}
-              type={brewery.brewery_type}
-              city={brewery.city}
-            />
-          ))}
-        </div>
-      ))}
+    <div className='wrapper'>
+      <div className='page-container'>
+        <h2>All Breweries</h2>
+        <FilterBar breweries={breweries} onFilter={handleFilter} />
+        {Object.keys(groupedBreweries).sort().map(char => (
+          <div key={char}>
+            <h2>{char.match(/[A-Z]/) ? char : '0-9'}</h2>
+            {groupedBreweries[char].map(brewery => (
+              <Link to={`/detail/${brewery.id}`} key={brewery.id}>
+                <BrewCard
+                  name={brewery.name}
+                  type={brewery.brewery_type}
+                  city={brewery.city}
+                />
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
