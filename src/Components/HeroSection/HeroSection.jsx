d30023ea368a649ca65cdbfd75511d587daf5fdf
Adam Konber <konber3@gmail.com>
2024-08-01 13:38:03 -0600

    Added content and styles to HeroSection

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -3,7 +3,9 @@ import '../HeroSection/HeroSection.css'
 
 const HeroSection = () => {
   return (
-    <div className='hero-section'>HeroSection</div>
+    <div className='hero-section'>
+      <h1>Have No Fear! The Beer is HERE!</h1>
+    </div>
   )
 }
 
