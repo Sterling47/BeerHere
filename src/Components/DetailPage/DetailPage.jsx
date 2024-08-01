aa9c0d6b40517a6e104128e46f81eacb58057fbc
Adam Konber <konber3@gmail.com>
2024-08-01 13:53:28 -0600

    Created Functionality to get specific details using useParams

--- src/Components/DetailPage/DetailPage.jsx
+++ src/Components/DetailPage/DetailPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import '../DetailPage/DetailPage.css'
 
 const DetailPage = ({ breweries }) => {
-    const { id } = useParams();
-    console.log('id', id)
-    const singleBrewery = breweries.find(brewery => brewery.id === id)
+  const { id } = useParams();
+  console.log('id', id)
+  const singleBrewery = breweries.find(brewery => brewery.id === id)
 
-    if (!singleBrewery) {
-        return <div>Brewery not found</div>;
-      }
-      const { name, brewery_type, address_1, address_2, address_3, city, state, postal_code, country, phone, website_url } = singleBrewery;
+  if (!singleBrewery) {
+    return <div>Brewery not found</div>;
+  }
+  const { name, brewery_type, address_1, address_2, address_3, city, state, postal_code, country, phone, website_url } = singleBrewery;
   return (
-    <div>{name}
-    {brewery_type}</div>
+    <div className='container'>
+      <div className='detail-section'>
+        <h2>{name}</h2>
+        <p>Brewery Type: {brewery_type}</p>
+        <p>This brewery is Located in {city}, {state}</p>
+        <p>Main Location: {address_1}, {city}, {state} - {postal_code}</p>
+        <p>Phone: {phone}</p>
+      </div>
+    </div>
   )
 }
 
