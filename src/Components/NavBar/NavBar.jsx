682d8367d40062cdd5831dea68d0b6893d6e6273
Adam Konber <konber3@gmail.com>
2025-02-02 15:35:31 -0700

    Updated beerLogo image src to be imported

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import {React} from 'react'
 import { Link } from 'react-router-dom'
 import '/src/Components/NavBar/Navbar.css'
+import beerLogo from '/src/assets/beerherelogo.png'
+
 const NavBar = () => {
     return (
         <nav className='navbar'>
             <div>
-            <Link to='/'><img className='logo' src='src/assets/beerherelogo.png'/></Link>
+            <Link to='/'><img className='logo' src={beerLogo}/></Link>
             </div>
            <Link className='bttn' to='/AllBreweries'>All Breweries</Link>
         </nav>
